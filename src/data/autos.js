335cdc5c6ae528dcf76e8f692248fce5919c0f5a
Martina491 <marticabrera28@gmail.com>
2024-02-23 18:16:36 -0300

    descripcion de arrays

--- src/data/autos.js
+++ src/data/autos.js
@@ -2,7 +2,25 @@ const autos = [
     {
         name: " Ford Mustang Eleanor Pony",
         description: "El Ford Mustang Eleanor es un ícono de potencia y elegancia en el mundo de los automóviles clásicos. Con su distintivo diseño de carrocería fastback y su inconfundible esquema de colores plateado y negro, Eleanor se alza como un símbolo de estilo y velocidad.Bajo el capó, su corazón late con un poderoso motor V8 que ruge con una potencia impresionante, haciendo que cada arranque sea una experiencia emocionante.Eleanor no es solo un automóvil; es una leyenda viviente que desafía el tiempo y sigue dejando una estela de admiración en su estela, cautivando a quienes tienen el privilegio de verla en acción",
-        
+        image: "./multimedia/pony3.jpg",
+    },
 
-    }
+    {
+        name: "Jaguar E-Type 60",
+        description: "Este vehículo no es solo una máquina de velocidad, sino un viaje en el tiempo que evoca la elegancia de una era pasada.El E- Type 60 Collection es una pieza de la historia automovilística que sigue inspirando admiración y respeto en quienes tienen el privilegio de contemplarlo.Es un homenaje a la pasión y la maestría que hace que Jaguar siga siendo un ícono del automovilismo.",
+        image: "./multimedia/jaguar1.jpg",
+    },
+
+    {
+        name: "Aston Martin",
+        description: "El Aston Martin, un símbolo de lujo y elegancia, se erige como una obra maestra de la ingeniería y el diseño automovilístico. Este vehículo es la personificación del estilo británico clásico y la potencia pura. Su carrocería aerodinámica y sus líneas fluidas encarnan la sofisticación, mientras que su motor potente es la fuerza detrás de una experiencia de conducción verdaderamente emocionante. El Aston Martin, un símbolo de lujo y elegancia, se erige como una obra maestra de la ingeniería y el diseño automovilístico. Este vehículo es la personificación del estilo británico clásico y la potencia pura. Su carrocería aerodinámica y sus líneas fluidas encarnan la sofisticación, mientras que su motor potente es la fuerza detrás de una experiencia de conducción verdaderamente emocionante.",
+        image: "./multimedia/aston2.jpg",
+    },
+
+
+    {
+        name: "Lamborghini Miura Jota",
+        description: "El Lamborghini Miura Jota, un icono atemporal en el mundo de los superdeportivos, es una máquina que fusiona poder y elegancia en una sinfonía de velocidad. Creado a partir del ya impresionante Lamborghini Miura, el Miura Jota eleva aún más la apuesta en términos de rendimiento y exclusividad. Este superdeportivo italiano es conocido por su diseño espectacular, con líneas agresivas y un perfil bajo que lo hacen destacar entre la multitud. Bajo el capó, un motor V12 rugiente entrega una potencia impresionante, llevando al Miura Jota a velocidades que desafían la gravedad en un abrir y cerrar de ojos.Dentro de su cabina, el lujo y la artesanía se unen en perfecta armonía.Cada detalle, desde los asientos de cuero hasta los acabados de aluminio, refleja la atención meticulosa al diseño y la calidad. El Miura Jota es un recordatorio de que los automóviles no solo pueden ser vehículos de transporte, sino también obras de arte en movimiento.",
+    image: "./multimedia/jota3.jpg",
+    },
 ] 
