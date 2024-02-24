f7e10a3392a296e3a85f8a9b07889c057b971aff
Martina491 <marticabrera28@gmail.com>
2024-02-24 14:14:43 -0300

    renderizando carrito

---
+++ src/componentes/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { addToCart, updateCartCount } from './carrito'; 
+
+const CartWidget = () => {
+    return (
+        <div className="carrito-widget">
+            <button onClick={addToCart}>Agregar al carrito</button>
+            <span id="carrito-count">0</span>
+        </div>
+    );
+};
+
+export default CartWidget;
